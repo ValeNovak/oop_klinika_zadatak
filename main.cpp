131e6e7ed42877b06deba663806d659cd0d06ffa
Leno <leno.development@outlook.com>
2026-06-16 17:06:17 +0200

    Izbornik

--- main.cpp
+++ main.cpp
@@ -9,11 +9,49 @@
 /* run this program using the console pauser or add your own getch,
  * system("pause") or input loop */
 
+void dodajDoktora(Recepcija *recepcija) {
+  std::string ime;
+  std::string prezime;
+  std::string email;
+  std::string oib;
+
+  std::cout << "Ime ";
+  std::cin >> ime;
+
+  std::cout << "Prezime ";
+  std::cin >> ime;
+
+  std::cout << "Email ";
+  std::cin >> ime;
+
+  std::cout << "Oib";
+  std::cin >> ime;
+}
+
 int main(int argc, char **argv) {
 
   Vremenska_oznaka v;
   Datum datum(6, 3, 2026);
   v.postaviVrijeme(datum, 13, 0);
+
+  int izbornik = 1;
+
+  while (izbornik != 0) {
+
+    std::cout << "Izaberi : " << std::endl;
+    std::cout << "Dodaj doktora - 1" << std::endl;
+    std::cout << "Dodaj pacijenta - 2" << std::endl;
+    std::cout << "Rezerviraj termin - 3" << std::endl;
+    std::cin >> izbornik;
+
+    switch (izbornik) {
+
+    case 1: {
+
+      break;
+    }
+    }
+  }
 
   Klinika klinika;
 
